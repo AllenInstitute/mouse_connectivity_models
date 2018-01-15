26069d5931eafdf45d73c90498eb5ab55246f32e
Joseph Knox <jknox13@uw.edu>
2018-01-15 14:32:29 -0800

    get_experiment_ids function

--- voxel_model/utils.py
+++ voxel_model/utils.py
@@ -1,7 +1,15 @@
 # Authors: Joseph Knox josephk@alleninstitute.org
-# License: 
+# License:
+
+def get_experiment_ids(mcc, structure_ids):
+    """Returns all experiment ids given some structure_ids"""
+    experiments = mcc.get_experiments(dataframe=True,
+                                      injection_structure_ids=structure_ids)
+    return experiments.injection_structure_ids
+
 
 def get_id_acronym_map(mcc):
     """Returns dict id : acronym"""
-    acronym_map = mcc.get_structure_tree().value_map(lambda x: x['id'], 
-                                                       lambda x: x['acronym'])
+    acronym_map = mcc.get_structure_tree().value_map(lambda x: x['id'],
+                                                     lambda x: x['acronym'])
+    return acronym_map
