f6b3e963011eccebd04acf32de8319e9d4c9edf5
Joseph Knox <jknox13@uw.edu>
2018-01-15 14:46:37 -0800

    basic test for get_model_data

--- voxel_model/test/test_experiment.py
+++ voxel_model/test/test_experiment.py
@@ -7,7 +7,9 @@ import numpy as np
 from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
 from allensdk.test_utilities.temp_dir import md_temp_dir
 
-from voxel_model.experiment import Experiment
+from voxel_model.experiment import Experiment, get_model_data
+from voxel_model.masks import SourceMask, TargetMask
+from voxel_model.utils import get_experiment_ids
 
 @pytest.fixture(scope="module")
 def mcc(md_temp_dir):
@@ -18,30 +20,39 @@ def mcc(md_temp_dir):
                                   ccf_version="annotation/ccf_2017")
 
 @pytest.fixture(scope="module")
-def experiment_id(mcc):
+def experiment_id():
     return 158374671
 
-@pytest.fixture(scope="function")
-def experiment(mcc, experiment_id):
-    return Experiment(mcc, experiment_id)
+@pytest.fixture(scope="module")
+def structure_ids():
+    return [512]
+
+@pytest.fixture(scope="module")
+def hemisphere():
+    return 3
 
 # ----------------------------------------------------------------------------
-# test 
-def test_mask_to_valid(experiment):
+# test
+def test_mask_to_valid():
     pass
 
 # ----------------------------------------------------------------------------
-# test 
-def test_injection_density(experiment):
-    """Covers injection_fraction and projection_density"""
-    pass
+# test
+def test_get_model_data(mcc, structure_ids, hemisphere):
+    # get masks
+    source_mask = SourceMask(mcc, structure_ids=structure_ids)
+    target_mask = TargetMask(mcc, structure_ids=structure_ids,
+                             hemisphere=hemisphere)
 
-# ----------------------------------------------------------------------------
-# test 
-def test_normalized_projection_density(experiment):
-    pass
+    # get experiment ids
+    experiment_ids = get_experiment_ids(mcc, structure_ids)
 
-# ----------------------------------------------------------------------------
-# test 
-def test_centroid(experiment):
-    pass
+    centroids, injections, projections = get_model_data(mcc,
+                                                        experiment_ids,
+                                                        source_mask,
+                                                        target_mask)
+
+    # check shape
+    assert( centroids.shape[0] == injections.shape[0] )
+    assert( injections.shape[0] == projections.shape[0] )
+    assert( projections.shape[0] == centroids.shape[0] )
