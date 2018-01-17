dba4c72bf9026799dcec7a205175b0955345dd93
Joseph Knox <jknox13@uw.edu>
2018-01-16 17:47:34 -0800

    fix tests

--- voxel_model/masks.py
+++ voxel_model/masks.py
@@ -6,7 +6,16 @@
 from __future__ import division
 import numpy as np
 
-def union_mask(mcc, structure_ids):
+# only R hemisphere for source
+_HEMISPHERES = [1,2,3]
+_SOURCE_HEMISPHERE = 2
+_TARGET_HEMI_MAP = {
+    "ipsi" : 2, #right
+    "contra" : 1, #left
+    "both" : 3
+}
+
+def union_mask(mcc, structure_ids, return_key=False):
     """Returns the union of a set of structure masks.
 
     Parameters
@@ -18,32 +27,36 @@ def union_mask(mcc, structure_ids):
     structure_ids : array-like, optional, shape (n_structure_ids,)
         AllenSDK CCF Annotation stucture ids to be included in the model
 
+    return_key : bool, optional (default=False)
+        Return mask as a key (each element in array is structure id) (True)
+        or as binary mask (False)
+
     Returns
     -------
-    C : array-like, shape=(x_ccf, y_ccf, z_ccf)
+    union : array-like, shape=(x_ccf, y_ccf, z_ccf)
         Union of structure_masks from each structure_id
     """
-    masks = [ mcc.get_structure_mask(structure_id)[0]
-              for structure_id in structure_ids ]
-    return np.logical_or.reduce(masks)
-
-def union_key(mcc, structure_ids):
-    # NOTE: TERRIBLE NAME
-    """Union of keys.
-    """
-
     # TODO: could check that structures are not decendents of eachother
 
+    def pull_mask(sid):
+        """Pulls mask. helper"""
+        mask = mcc.get_structure_mask(sid)[0]
+        if return_key:
+            np.multiply(mask, sid, mask)
+
+        return mask
+
     # fencepost
-    sid = structure_ids[0]
-    key = sid*mcc.get_structure_mask(sid)[0]
+    union = pull_mask(structure_ids[0])
 
     if len(structure_ids) > 1:
-        for sid in structure_ids:
-            # structures should be non overlapping!!!!
-            np.add(key, sid*mcc.get_structure_mask(sid)[0], key)
+        for sid in structure_ids[1:]:
+            mask = pull_mask(sid)
 
-    return key
+            # structures should be non overlapping!!!!
+            np.add(union, mask, union)
+
+    return union
 
 class _BaseMask(object):
     """Base Mask class for SourceMask and TargetMask.
@@ -72,13 +85,13 @@ class _BaseMask(object):
     def __init__(self, mcc, structure_ids, hemisphere):
         self.mcc = mcc
         self.structure_ids = structure_ids
-        self.hemishpere = hemisphere
+        self.hemisphere = hemisphere
 
-    def _get_mask(self):
+    def _get_mask(self, return_key):
         """   """
-        mask = union_mask(self.mcc, self.structure_ids)
-        midline = mask.shape[2]//2
+        mask = union_mask(self.mcc, self.structure_ids, return_key=return_key)
 
+        midline = mask.shape[2]//2
         if self.hemisphere == 1:
             # contra
             mask[:,:,:midline] = False
@@ -93,7 +106,7 @@ class _BaseMask(object):
         try:
             return self._mask
         except AttributeError:
-            self._mask = self._get_mask()
+            self._mask = self._get_mask(return_key=False)
             return self._mask
 
     @property
@@ -114,33 +127,16 @@ class _BaseMask(object):
 #     def key(self):
 #         """Returns nonzero indices of mask"""
 #         return self.mask.flatten().nonzero()[0]
-    def _get_key(self):
-        """   """
-        key = union_key(self.mcc, self.structure_ids)
-        midline = key.shape[2]//2
-
-        if self.hemisphere == 1:
-            # contra
-            key[:,:,:midline] = False
-        elif self.hemisphere == 2:
-            # ipsi
-            key[:,:,midline:] = False
-
-        return key.flatten().nonzero()[0]
 
     @property
     def key(self):
         try:
-            return self._key
+            key = self._key
         except AttributeError:
-            self._key = self._get_key()
-            return self._key
+            self._key = self._get_mask(return_key=True)
+            key = self._key
 
-
-    @property
-    def key(self):
-        """Returns the key ... """
-
+        return key.flatten().nonzero()[0]
 
     def map_to_ccf(self, y):
         """Maps a masked vector y back to ccf
@@ -163,36 +159,23 @@ class _BaseMask(object):
 
         return y_ccf.reshape(self.ccf_shape)
 
-
 class SourceMask(_BaseMask):
     """Mask for source
     """
-
-    # enforces R hemisphere injection
-    hemisphere = 2
-
     def __init__(self, mcc, structure_ids):
-        super(SourceMask, self).__init__(mcc, structure_ids, self.hemisphere)
+        super(SourceMask, self).__init__(mcc, structure_ids, _SOURCE_HEMISPHERE)
 
 class TargetMask(_BaseMask):
     """Mask for target
     """
-
-    _hemi_map = {
-        "ipsi" : 2, #right
-        "contra" : 1, #left,
-        "both" : 3
-    }
-
-    def __init__(self, mcc, structure_ids, hemisphere):
-        try:
-            self.hemisphere = self._hemi_map[hemisphere]
-        except KeyError:
-            if hemisphere in range(1,4):
-                self.hemisphere = hemisphere
-            else:
+    # TODO: change to classmethod constructor
+    def __init__(self, mcc, structure_ids, hemisphere=3):
+        if hemisphere not in _HEMISPHERES:
+            try:
+                hemisphere = _TARGET_HEMI_MAP[hemisphere]
+            except KeyError:
                 raise ValueError("must pass {} or {} to hemisphere".format(
-                    self._hemi_map.keys(), self._hemi_map.values()
-                ))
+                        _TARGET_HEMI_MAP.keys(), _HEMISPHERES
+                    ))
 
         super(TargetMask, self).__init__(mcc, structure_ids, hemisphere)
--- voxel_model/test/test_masks.py
+++ voxel_model/test/test_masks.py
@@ -1,67 +1,99 @@
 from __future__ import division
-import os
 import mock
+import copy
 import pytest
 import numpy as np
 
-from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
-from allensdk.test_utilities.temp_dir import md_temp_dir
-
-from voxel_model.masks import SourceMask, union_mask
-
-@pytest.fixture(scope="module")
-def mcc(md_temp_dir):
-
-    manifest_path = os.path.join(md_temp_dir, "manifest.json")
-    return MouseConnectivityCache(manifest_file=manifest_path,
-                                  resolution=100,
-                                  ccf_version="annotation/ccf_2017")
+from itertools import cycle
+from numpy.testing import assert_array_equal
+from voxel_model.masks import SourceMask, TargetMask, union_mask
 
 @pytest.fixture(scope="module")
-def ccf_shape(mcc):
-    return mcc.get_structure_mask(315)[0].shape
+def structure_ids():
+    # arbitrary
+    return [101, 310]
 
-@pytest.fixture(scope="module")
-def structure_ids(mcc):
-    return [315, 313, 512]
+@pytest.fixture(scope="function")
+def mcc():
+    mask_r = ( np.array([[[1,1],[1,0]],[[1,0],[0,0]]]), )
+    mask_l = ( np.array([[[0,0],[0,1]],[[0,1],[1,1]]]), )
+
+    mcc = mock.Mock()
+    mcc.get_structure_mask.side_effect = cycle([mask_r, mask_l])
+    return mcc
 
 @pytest.fixture(scope="function")
 def source_mask(mcc, structure_ids):
     return SourceMask(mcc, structure_ids)
 
-def test_union_mask(mcc, structure_ids, ccf_shape):
-    umask = union_mask(mcc, structure_ids)
+# -----------------------------------------------------------------------------
+# tests
+def test_union_mask(mcc):
+    structure_ids = [101, 310] #arbitrary
+    mask = union_mask(mcc, structure_ids, return_key=False)
+    key = union_mask(mcc, structure_ids, return_key=True)
 
-    assert( umask.shape == ccf_shape )
-    assert( umask.dtype == bool )
+    # mask should cover whole
+    assert_array_equal( mask, np.ones((2,2,2)) )
 
+    # same coverage
+    assert_array_equal( (mask > 0), (key > 0) )
+
+    # keys should only be sids above
+    assert_array_equal( np.unique(keys), structure_ids )
+
+# _BaseMask, SourceMask
+# -----------------------------------------------------------------------------
+# tests
 def test_get_mask(source_mask):
-    mask = source_mask.mask
-    midline = source_mask.ccf_shape[2]//2
-
-    left_hemi = mask[:,:,midline:]
+    left_hemi = source_mask.mask[:,:,1:]
 
     assert( left_hemi.sum() == 0 )
 
+# -----------------------------------------------------------------------------
+# tests
+def test_ccf_shape(source_mask):
 
-def test_ccf_shape(ccf_shape, source_mask):
+    assert( source_mask.ccf_shape == (2,2,2) )
 
-    assert( source_mask.ccf_shape == ccf_shape )
-
+# -----------------------------------------------------------------------------
+# tests
 def test_coordinates(source_mask):
     shape = source_mask.coordinates.shape
 
     assert( shape[1] == 3 )
     assert( shape[0] == int(source_mask.mask.sum()) )
 
+# -----------------------------------------------------------------------------
+# tests
 def test_key(source_mask):
     shape = source_mask.key.shape
 
     assert( len(shape) == 1 )
 
+# -----------------------------------------------------------------------------
+# tests
 def test_map_to_ccf(source_mask):
     """NOT SUFFICIENT"""
-    y = np.ones_like(source_mask.key)
     mask_ = source_mask.mask.astype(np.int)
+    y = np.ones_like(source_mask.key)
 
-    assert( np.array_equal(source_mask.map_to_ccf(y), source_mask.mask) )
+    assert_array_equal( source_mask.map_to_ccf(y), source_mask.mask )
+
+# TargetMask
+# -----------------------------------------------------------------------------
+# tests constructor
+# TODO: change to classmethod constructor
+def test_target_mask(mcc, structure_ids):
+    ipsi = TargetMask(mcc, structure_ids, "ipsi")
+    contra = TargetMask(mcc, structure_ids, "contra")
+    both = TargetMask(mcc, structure_ids, "both")
+
+    # constructor
+    assert( ipsi.hemisphere == 2 )
+    assert( contra.hemisphere == 1 )
+    assert( both.hemisphere == 3 )
+
+    # hemi masks
+    assert( ipsi.mask[:,:,1:].sum() == 0 )
+    assert( contra.mask[:,:,:1].sum() == 0 )
