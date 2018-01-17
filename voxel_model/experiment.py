ca69b579c8665222aaba88eacb5db85de47cb3a3
Joseph Knox <jknox13@uw.edu>
2018-01-16 16:18:01 -0800

    move model_data to utils

--- voxel_model/experiment.py
+++ voxel_model/experiment.py
@@ -1,14 +1,9 @@
 # Authors: Joseph Knox josephk@alleninstitute.org
 # License:
-
-# NOTE : ModelData NOT complete!!
-
-from __future__ import division, absolute_import
+from __future__ import division
 import numpy as np
 
 from allensdk.api.queries.mouse_connectivity_api import MouseConnectivityApi
-
-from .masks import SourceMask, TargetMask
 
 class Experiment(object):
     """Class containing the data from an anterograde injection
@@ -112,6 +107,9 @@ class Experiment(object):
     def projection_density(self):
         if self.normalize_projection:
             total_injection_volume = self.injection_density.sum()
+            if total_injection_volume <= 0.0:
+                raise ValueError("total injection volume is zero!!")
+
             return self._projection_density/total_injection_volume
         else:
             return self._projection_density
@@ -121,45 +119,3 @@ class Experiment(object):
         return MouseConnectivityApi().calculate_injection_centroid(
             self.injection_density, self.injection_fraction, resolution=1
         )
-
-
-def get_model_data(mcc, experiment_ids, source_mask, target_mask,
-                   normalize_projection=False):
-    """Function for gettting data for model.
-
-    Parameters
-    ----------
-    mcc : allensdk.core.mouse_connectivity_cache.MouseConnectivityCache object
-        This supplies the interface for pulling experimental data
-        from the AllenSDK.
-
-    experiment_ids : int, optional, shape (n_experiment_ids,)
-        AllenSDK id assigned to experiments to be included in the model
-
-    source_mask : voxel_model.masks.source_mask object
-        Defines the mask of the voxels included in the source in the model.
-
-    target_mask : voxel_model.masks.target_mask object
-        Defines the mask of the voxels included in the target in the model.
-
-    Returns
-    -------
-    """
-    centroids = []
-    injections = []
-    projections = []
-    for eid in experiment_ids:
-        # get experiment data
-        experiment = Experiment(mcc, eid, normalize_projection=normalize_projection)
-
-        # mask injection/projection to source/target masks
-        ind = experiment.injection_density[source_mask.where]
-        prd = experiment.projection_density[target_mask.where]
-
-        # append relevant attrs
-        centroids.append(experiment.centroid)
-        injections.append(ind)
-        projections.append(prd)
-
-    # return as arrays
-    return np.asarray(centroids), np.asarray(injections), np.asarray(projections)
