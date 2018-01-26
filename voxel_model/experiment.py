3f15b5265d9e82e75e209c0d54d8350e9b79b277
Joseph Knox <jknox13@uw.edu>
2018-01-26 11:40:47 -0800

    from_mcc and new take kwarg injection_hemisphere

--- voxel_model/experiment.py
+++ voxel_model/experiment.py
@@ -61,10 +61,9 @@ class Experiment(namedtuple("Experiment", ["injection_density",
     __slots__ = ()
 
     DATA_MASK_TOLERANCE = 0.5
-    INJECTION_HEMISPHERE = 2
 
     @classmethod
-    def from_mcc(cls, mcc, experiment_id):
+    def from_mcc(cls, mcc, experiment_id, injection_hemisphere=2):
         # pull data
         data_mask = mcc.get_data_mask(experiment_id)[0]
         injection_density = mcc.get_injection_density(experiment_id)[0]
@@ -79,21 +78,23 @@ class Experiment(namedtuple("Experiment", ["injection_density",
         injection_density[ data_mask < cls.DATA_MASK_TOLERANCE ] = 0.
 
         return cls(injection_density=injection_density,
-                   projection_density=projection_density)
+                   projection_density=projection_density,
+                   injection_hemisphere=injection_hemisphere)
 
     @staticmethod
     def _get_injection_hemisphere(injection_density):
         """Gets injection hemisphere based on injection density."""
 
         # split along depth dimension (forces arr.shape[2] % 2 == 0)
-        l_hemi, r_hemi = np.dsplit(injection_density, 2)
+        l_hemi, r_hemi = np.split(injection_density, 2, axis=2)
 
         if l_hemi.sum() > r_hemi.sum():
             return 1
         else:
             return 2
 
-    def __new__(cls, injection_density=None, projection_density=None):
+    def __new__(cls, injection_density=None, projection_density=None,
+                injection_hemisphere=2):
         if ( type(injection_density) == np.ndarray and
                 type(projection_density) == np.ndarray ):
 
@@ -106,13 +107,17 @@ class Experiment(namedtuple("Experiment", ["injection_density",
                               "must be of type numpy.ndarray" )
 
 
-        # check injection hemisphere
-        injection_hemisphere = cls._get_injection_hemisphere(injection_density)
+        if injection_hemisphere in [1,2]:
+            # check injection hemisphere
+            computed_hemi = cls._get_injection_hemisphere(injection_density)
 
-        if injection_hemisphere != cls.INJECTION_HEMISPHERE:
-            # flip experiment
-            injection_density = np.flip( injection_density, axis=2 )
-            projection_density = np.flip( projection_density, axis=2 )
+            if injection_hemisphere != computed_hemi:
+                # flip experiment
+                injection_density = np.flip( injection_density, axis=2 )
+                projection_density = np.flip( projection_density, axis=2 )
+
+        elif injection_hemisphere != 3:
+            raise ValueError( "injection_hemisphere must be 1, 2, or 3" )
 
         return super(Experiment, cls).__new__( cls, injection_density,
                                                projection_density )
--- voxel_model/test/test_experiment.py
+++ voxel_model/test/test_experiment.py
@@ -9,56 +9,31 @@ from allensdk.api.queries.mouse_connectivity_api import MouseConnectivityApi
 
 from voxel_model.experiment import Experiment
 
-@pytest.fixture(scope="module")
-def shape():
-    return (10,10,10)
-
-@pytest.fixture(scope="module")
-def data_mask(shape):
-    return np.ones(shape)
-
-@pytest.fixture(scope="module")
-def injection_density(shape):
-    return np.ones(shape)
-
-@pytest.fixture(scope="module")
-def injection_fraction(injection_density):
-    return injection_density != 0
-
-@pytest.fixture(scope="module")
-def projection_density(shape):
-    return np.ones(shape)
-
-@pytest.fixture(scope="module")
-def mcc(data_mask, injection_density, injection_fraction, projection_density):
-
-    mcc = mock.Mock()
-
-    mcc.get_data_mask.return_value = (data_mask, )
-    mcc.get_injection_density.return_value = (injection_density, )
-    mcc.get_injection_fraction.return_value = (injection_fraction, )
-    mcc.get_projection_density.return_value = (projection_density, )
-
-    mcc.api.return_value = MouseConnectivityApi()
-
-    return mcc
-
 # -----------------------------------------------------------------------------
 # tests
-def test_from_mcc(mcc, injection_density, injection_fraction, projection_density):
-    experiment_id = 1023223
+def test_from_mcc(mcc):
 
-    data = Experiment.from_mcc(mcc, experiment_id)
+    # pull 'data' from mcc fixture
+    experiment_id = 1023223
+    injection_density = mcc.get_injection_density(experiment_id)[0]
+    injection_fraction = mcc.get_injection_fraction(experiment_id)[0]
+    projection_density = mcc.get_projection_density(experiment_id)[0]
 
     # 'true' injection density from constructor
     np.multiply(injection_density, injection_fraction, injection_density)
 
-    assert_array_equal( data.injection_density, injection_density )
-    assert_array_equal( data.projection_density, projection_density )
+    experiment = Experiment.from_mcc(mcc, experiment_id)
+
+    assert_array_equal( experiment.injection_density, injection_density )
+    assert_array_equal( experiment.projection_density, projection_density )
 
 # -----------------------------------------------------------------------------
 # tests
-def test_get_injection_hemisphere(injection_density, projection_density):
+def test_get_injection_hemisphere(mcc):
+    # pull 'data' from mcc fixture
+    experiment_id = 1023223
+    injection_density = mcc.get_injection_density(experiment_id)[0]
+    projection_density = mcc.get_projection_density(experiment_id)[0]
 
     # left injection
     l_inj = np.copy(injection_density)
@@ -79,45 +54,52 @@ def test_get_injection_hemisphere(injection_density, projection_density):
 
 # -----------------------------------------------------------------------------
 # tests
-def test_sum_injection(mcc, injection_density, injection_fraction):
+def test_sum_injection(mcc):
+    # pull 'data' from mcc fixture
     experiment_id = 1023223
-
-    data = Experiment.from_mcc(mcc, experiment_id)
+    injection_density = mcc.get_injection_density(experiment_id)[0]
+    injection_fraction = mcc.get_injection_fraction(experiment_id)[0]
 
     # 'true' injection density from constructor
     np.multiply(injection_density, injection_fraction, injection_density)
 
-    assert( data.injection_density.sum() == injection_density.sum() )
+    experiment = Experiment.from_mcc(mcc, experiment_id)
+
+    assert( experiment.injection_density.sum() == injection_density.sum() )
 
 # -----------------------------------------------------------------------------
 # tests
-def test_compute_centroid(mcc, injection_density, injection_fraction):
-
+def test_compute_centroid(mcc):
+    # pull 'data' from mcc fixture
     experiment_id = 1023223
-    data = Experiment.from_mcc(mcc, experiment_id)
+    injection_density = mcc.get_injection_density(experiment_id)[0]
+    injection_fraction = mcc.get_injection_fraction(experiment_id)[0]
 
+    # compute allensdk centroid
     api = MouseConnectivityApi()
     mcc_centroid = api.calculate_injection_centroid( injection_density,
                                                      injection_fraction,
                                                      resolution=1 )
 
-    assert_array_equal( data.centroid, mcc_centroid )
+    experiment = Experiment.from_mcc(mcc, experiment_id)
+
+    assert_array_equal( experiment.centroid, mcc_centroid )
 
 # -----------------------------------------------------------------------------
 # tests
 def test_normalized_injection_density(mcc):
 
     experiment_id = 1023223
-    data = Experiment.from_mcc(mcc, experiment_id)
+    experiment = Experiment.from_mcc(mcc, experiment_id)
 
-    assert( data.normalized_injection_density.dtype == np.float )
+    assert( experiment.normalized_injection_density.dtype == np.float )
 
 # -----------------------------------------------------------------------------
 # tests
 def test_normalized_projection_density(mcc):
 
     experiment_id = 1023223
-    data = Experiment.from_mcc(mcc, experiment_id)
+    experiment = Experiment.from_mcc(mcc, experiment_id)
 
-    assert( data.normalized_projection_density.dtype == np.float )
+    assert( experiment.normalized_projection_density.dtype == np.float )
 
