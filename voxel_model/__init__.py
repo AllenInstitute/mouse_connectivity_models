1c45abccdad71a01981879f061a885680898027c
Joseph Knox <jknox13@uw.edu>
2018-03-29 13:04:55 -0700

    toplevel init

--- voxel_model/__init__.py
+++ voxel_model/__init__.py
@@ -4,3 +4,17 @@
 # License: 
 
 __version__ = '0.0.1'
+
+from . import core
+from . import regressors
+from . import stats
+from . import regionalized_model
+from . import utils
+from . import voxel_connectivity_array
+
+__all__ = ['core',
+           'regressors',
+           'stats',
+           'regionalized_model',
+           'utils',
+           'voxel_connectivity_array']
