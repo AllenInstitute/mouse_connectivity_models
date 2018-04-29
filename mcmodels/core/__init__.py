19678f6bad1b42a2b599a6553a97f006cd10c1ba
Joseph Knox <jknox13@uw.edu>
2018-04-29 10:46:45 -0700

    update __init__ to reflect Api/Cache changes

--- mcmodels/core/__init__.py
+++ mcmodels/core/__init__.py
@@ -4,18 +4,18 @@ The :mod:`mcmodels.core` module implements objects useful in data manipulation.
 # Authors: Joseph Knox <josephk@alleninstitute.org>
 # License: Allen Institute Software License
 
-from .voxel_model_api import VoxelModelApi
-from .voxel_model_cache import VoxelModelCache
 
 from .base import VoxelData, RegionalData
 from .masks import Mask
 from .experiment import Experiment
+from .voxel_model_api import VoxelModelApi
+from .voxel_model_cache import VoxelModelCache
 from . import utils
 
 __all__ = ['Experiment',
            'Mask',
            'RegionalData',
            'VoxelData',
+           'VoxelModelApi',
            'VoxelModelCache',
-           'cache',
            'utils']
